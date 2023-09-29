f6c6ed7956b5904ac9750df804fd9d7bf7068544
Merseyside <ivanklessablin@gmail.com>
2023-09-29 17:30:07 +0600

    Building changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 allprojects {
     plugins.withId("org.gradle.maven-publish") {
         group = "io.github.merseyside"
-        version = "0.1.0"
+        version = multiplatformLibs.versions.mersey.messaging.get()
     }
 }
 
@@ -9,19 +9,3 @@ tasks.register("clean", Delete::class).configure {
     group = "build"
     delete(rootProject.buildDir)
 }
-//
-//val javadocDisabledModules = listOf(
-//    "oauth-core",
-//    "oauth-android"
-//)
-
-//subprojects {
-//    gradle.taskGraph.whenReady {
-//        if (javadocDisabledModules.contains(this@subprojects.name)) {
-//            tasks.matching { it.name == "javaDocReleaseGeneration" }.configureEach {
-//                // See: https://youtrack.jetbrains.com/issue/KTIJ-19005/JDK-17-PermittedSubclasses-requires-ASM9-exception-multiple-times-per-second-during-analysis
-//                enabled = false
-//            }
-//        }
-//    }
-//}
